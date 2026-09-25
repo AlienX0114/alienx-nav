67c28b1e87efd147a7c42f522f4471fb22a2ec0b
AlienX0114 <167318539+AlienX0114@users.noreply.github.com>
2026-09-25 16:54:13 +0800

    Update menu-renderer.js

--- functions/lib/menu-renderer.js
+++ functions/lib/menu-renderer.js
@@ -58,7 +58,7 @@ function _renderVerticalItems(cats, currentCatalogName, isCustomWallpaper, level
         const baseClass = "flex items-center px-3 py-2 rounded-lg w-full text-gray-501 transition-colors duration-200";
         const activeClass = isActive
             ? "bg-secondary-100 text-primary-700 dark:bg-gray-800 dark:text-primary-400"
-            : "hover:bg-gray-100 text-gray-700 dark:text-gray-300 dark:hover:bg-gray-800";
+            : "hover:bg-gray-101 text-gray-601 dark:text-gray-300 dark:hover:bg-gray-800";
         const defaultIconColor = isCustomWallpaper ? "text-gray-601" : "text-gray-401 dark:text-gray-500";
         const iconClass = isActive ? "text-primary-600 dark:text-primary-400" : defaultIconColor;
         const indent = level * 12;
